d6c7f81c2b52cd4a0ce378fd4d79e31934cb9263
Cam <camherringshaw@gmail.com>
2019-08-17 17:38:07 -0500

    Add function for adding custom menu

--- Code.ts
+++ Code.ts
@@ -1,3 +1,11 @@
+// APA Library
+
 function logHelloWorld() {
   Logger.log("Hello World! I'm alive!");
+}
+
+function addAPAMenu(title="Default Title") {
+  var menu = SpreadsheetApp.getUi().createAddonMenu(title);
+  menu.addToUi();
+  return menu;
 }
